d9d6e440172fe5f6bf0fa98398d6bb45b9727375
Guillaume Gruber <lagrube1@gmail.com>
2021-04-14 22:38:48 +0200

    modif language

--- src/components/skill/Languages.js
+++ src/components/skill/Languages.js
@@ -4,16 +4,15 @@ import ProgressBar from "./ProgressBar";
 class Languages extends Component {
   state = {
     languages: [
-      { id: 1, value: "Css", xp: 2 },
+      { id: 1, value: "Sass", xp: 2 },
       { id: 2, value: "Javascript", xp: 1.8 },
       { id: 3, value: "MongoDB", xp: 1.4 },
       { id: 4, value: "Mysql", xp: 1.2 },
     ],
     frameworks: [
-      { id: 1, value: "Sass", xp: 1.8 },
-      { id: 2, value: "React", xp: 1.6 },
-      { id: 3, value: "Vue-js", xp: 1.03 },
-      { id: 4, value: "Express", xp: 1.4 },
+      { id: 1, value: "React", xp: 1.6 },
+      { id: 2, value: "Vue-js", xp: 1.03 },
+      { id: 3, value: "Express", xp: 1.4 },
       { id: 4, value: "Mongoose", xp: 1.4 },
     ],
   };
